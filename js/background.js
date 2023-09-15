ade9f36ee7a82169f59203146ce86f267a5cc383
hochoongkim <manapain@naver.com>
2023-09-15 16:01:17 +0900

    background

--- js/background.js
+++ js/background.js
@@ -1,9 +1,32 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const BACKGROUND_KEY = "background";
+const url = "https://cataas.com/cat?tags=cute&json=true&width=1000";
+const backgroundItem = document.querySelector(".background-item");
+const mainView = document.querySelector(".main-view");
+const skip = document.querySelector(".skip");
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+function getBackground() {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            saveBackground(data);
+            setBackground(data);
+        });
+}
 
-const bgImage = document.createElement("img");
+function setBackground(background) {
+    backgroundItem.style.backgroundImage = `url('https://cataas.com${background.url}')`;
+    mainView.classList.remove("hidden");
+}
 
-bgImage.src = `img/${chosenImage}`;
+function saveBackground(background) {
+    localStorage.setItem(BACKGROUND_KEY, JSON.stringify(background));
+}
 
-document.body.appendChild(bgImage);
+skip.addEventListener("click", getBackground);
+
+const savedBackground = localStorage.getItem(BACKGROUND_KEY);
+if (savedBackground !== null) {
+    setBackground(JSON.parse(savedBackground));
+} else {
+    getBackground();
+}
--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,6 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+const API_KEY = "6282aa5ec42dc74020f8f72248e5d164";
 
 function onGeoOk(position) {
     const lat = position.coords.latitude;
